587200a2f63c1d53b703ab4808359666285dc21b
Anucha Ananbenjapon <anucha_a@hotmail.com>
2021-10-12 16:51:55 +0700

    commit

--- examples/ch03/snippets_py/03_03.py
+++ examples/ch03/snippets_py/03_03.py
@@ -1,24 +1,31 @@
 # Section 3.3
-snippets
-grade = 85
+#snippets
+#grade = 85
 
-if grade >= 60:
-    print('Passed')
+grade = (int(input('Please input your grade:')))
+
+if grade >= 85:
+    print('excellent ')
 
 # Suite Indentation
 if grade >= 60:
-print('Passed')  # statement is not indented properly
+    print('Passed') 
+    print('Good Job!')
 
-if grade >= 60:
-    print('Passed')
-  print('Good job!)
+elif grade >= 50 and  grade <= 59:   # add by Anucha 
+    print('Passed') 
+   
+ 
+else:                                  # add by Anucha
+    print('Failed')
 
-# if Statement Flowchart
-if 1:
-    print('Nonzero values are true, so this will print')
     
-if 0:
-    print('Zero is false, so this will not print')
+# if Statement Flowchart
+#if 1:
+   # print('Nonzero values are true, so this will print')
+    
+#if 0:
+   # print('Zero is false, so this will not print')
 
 
 ##########################################################################
